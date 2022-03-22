e2bdbadbb920d83ee89f80896a7083b8ea5f435f
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2022-03-22 00:09:56 -0500

    🐛 incluir formato en listaVideos

--- crearListaVideos.js
+++ crearListaVideos.js
@@ -1,17 +1,17 @@
 const { readdir, writeFileSync } = require('fs');
 const { extname } = require('path');
-const formatos = ['.mp4', '.webm', '.ogv'];
+const formatos = ['mp4', 'webm', 'ogv'];
 const videos = [];
 
 readdir('publico/videos', (err, archivos) => {
   if (err) throw new Error(err);
 
   archivos.forEach((nombre) => {
-    const formato = extname(nombre);
+    const formato = extname(nombre).slice(1);
     const esVideo = formatos.includes(formato);
 
     if (esVideo) {
-      videos.push(nombre);
+      videos.push({ nombre, formato });
     }
   });
 
