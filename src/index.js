51b0276489a4732a6950dc6cfe2e5874bcce7aa9
3st3r30sc0p10 <h.idarraga@uniandes.edu.co>
2022-04-19 19:43:47 -0500

    implementación barra deslizadora y mejoras

--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ const contenedorMensaje = document.getElementById('mensaje');
 const lista = document.getElementById('listaVideos');
 const video = document.getElementById('video');
 const lienzo = document.getElementById('lienzo');
+const lienzo2 = document.getElementById('lienzo2');
 const ctx = lienzo.getContext('2d');
+const ctx2 = lienzo2.getContext('2d');
 const listaCategorias = document.getElementById('listaCategorias');
+const tiemposAparicionCategorias = document.getElementById('tiemposAparicionCategorias');
+const espacioAparicionCategorias = document.getElementById('espacioAparicionCategorias');
+const BarraDeRangos = document.getElementById('BarraDeRangos');
+const apariciones = {};
 
 let modelo;
 let contadorAnim;
@@ -53,52 +59,99 @@ video.onplay = () => {
 
 let listaCreadaCategorias = [];
 
+BarraDeRangos.oninput = function () {
+  return this.value;
+};
+
 async function verVideo() {
   if (video.readyState > 1) {
-    const predicciones = await modelo.detect(video, 20, 0.5);
+    const predicciones = await modelo.detect(video, 20, BarraDeRangos.value);
     ctx.clearRect(0, 0, lienzo.width, lienzo.height);
 
     predicciones.forEach((prediccion) => {
       const [x, y, ancho, alto] = prediccion.bbox;
       const { class: categoria } = prediccion;
-
       ctx.beginPath();
       ctx.rect(x, y, ancho, alto);
       ctx.stroke();
-
       ctx.save();
       ctx.fillStyle = 'black';
       ctx.fillRect(x, y, ctx.measureText(categoria).width, 20);
       ctx.fillStyle = 'white';
       ctx.fillText(categoria, x, y + 13);
       ctx.restore();
+      detector(prediccion);
+      // if (!listaCreadaCategorias.some((instancia) => instancia.categoria === categoria)) {
+      //   listaCreadaCategorias.push({
+      //     categoria,
+      //     apariciones: [],
+      //   });
+      //   const elemento = document.createElement('div');
+      //   elemento.className = 'categoria';
+      //   elemento.innerText = categoria;
+      //   listaCategorias.appendChild(elemento);
+      // }
 
-      if (!listaCreadaCategorias.some((instancia) => instancia.categoria === categoria)) {
-        listaCreadaCategorias.push({
-          categoria,
-          apariciones: [],
-        });
-        const elemento = document.createElement('p');
-        elemento.className = 'categoria';
-        elemento.innerText = categoria;
-        listaCategorias.appendChild(elemento);
-      }
+      // const objeto = listaCreadaCategorias.find((obj) => obj.categoria === categoria);
+      // objeto.apariciones.push({
+      //   tiempo: video.currentTime,
+      //   area: prediccion.bbox,
+      //   confianza: prediccion.score,
+      // });
 
-      const objeto = listaCreadaCategorias.find((obj) => obj.categoria === categoria);
-      objeto.apariciones.push({
-        tiempo: video.currentTime,
-        area: prediccion.bbox,
-      });
+      // const elementoTiempo = document.createElement('div');
+      // elementoTiempo.className = 'elementoTiempo';
+      // elementoTiempo.innerText = objeto.apariciones[0].tiempo;
+      // tiemposAparicionCategorias.appendChild(elementoTiempo);
+
+      // const elementoEspacio = document.createElement('div');
+      // elementoEspacio.className = 'elementoEspacio';
+      // elementoEspacio.innerText = objeto.apariciones[0].area;
+      // espacioAparicionCategorias.appendChild(elementoEspacio);
     });
   }
-  console.log(listaCreadaCategorias);
+
   contadorAnim = requestAnimationFrame(verVideo);
 }
+// function detector(prediccion, categoria) {
+//   if (!apariciones.hasOwnProperty(prediccion.class)) {
+//     apariciones[prediccion.class] = [];
+//   }
+//   apariciones[prediccion.class].push({
+//     tiempo: video.currentTime,
+//     area: prediccion.bbox,
+//     confianza: prediccion.score,
+//   });
+//   const categoriasDetectadas = Object.keys(apariciones);
+//   if (!listaCreadaCategorias.some((instancia) => instancia.prediccion === prediccion)) {
+//     listaCreadaCategorias.push({
+//       categoriasDetectadas,
+//     });
+//   }
+//   const elemento = document.createElement('div');
+//   elemento.className = 'categoria';
+//   elemento.innerText = listaCreadaCategorias;
+//   listaCategorias.appendChild(elemento);
+//   console.log(`prediccion=`, prediccion);
+// }
 
-function crearNuevoDiv(cat) {
-  let nuevoDiv = document.createElement('div');
-  div.innerHTML = cat;
-  return nuevoDiv;
+function detector(prediccion) {
+  if (!apariciones.hasOwnProperty(prediccion.class)) {
+    const nombreCategoria = prediccion.class;
+    apariciones[nombreCategoria] = [];
+    const elemento = document.createElement('div');
+    elemento.className = 'categoria';
+    elemento.innerText = nombreCategoria;
+    listaCategorias.appendChild(elemento);
+    elemento.onclick = () => {
+      console.log(`Apariciones de categoría ${prediccion.class}`, apariciones[nombreCategoria]);
+    };
+  }
+  apariciones[prediccion.class].push({
+    tiempo: video.currentTime,
+    area: prediccion.bbox,
+    confianza: prediccion.score,
+  });
 }
 
 async function inicio() {
